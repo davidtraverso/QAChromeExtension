c4157cdf39c7f67fabfa6044c94f9b68503431cb
davidtraverso <davidtraverso88@gmail.com>
2020-03-30 14:10:25 -0500

    version 1.3

--- content.js
+++ content.js
@@ -11,8 +11,10 @@ if (window.location.href.includes(qualPage)) {
   // Solution: add element outside of this wrapper
   // Solution: utilize the currently working 'sticky' navbar to add a new icon (utilize the environment and don't reinvent the wheel)
 
-  // Add icon to navbar
+  /* Add icon to navbar */
   var navList = document.querySelector('#icons_top ul');
+  // Fix width of parent <div> to prevent side effects
+  navList.parentNode.setAttribute('style', 'width:78%');
   var budgetItem = document.createElement('li');
   var iconSRC = 'https://software-advice.imgix.net/homepage/icon-brand_accounting.png';
   var budgetIcon = `<img id="quabity" src="${iconSRC}" alt="Show Budget" title="Show Budget" height="16" width="16">`;
@@ -27,17 +29,21 @@ if (window.location.href.includes(qualPage)) {
   function extractNotes(str) {
     var budgetField = document.getElementsByName(str);
     var note = '';
-    budgetField[0].value ? (note = budgetField[0].value) : (note = '** Empty Note **');
+    budgetField[0].value ? (note = budgetField[0].value) : (note = '---');
     return note;
   }
 
+  var userNote = extractNotes('cn_subsize2');
   var budgetNote = extractNotes('cn_budget');
   var budgetDiv = document.createElement('div');
   budgetDiv.id = 'quabity_note';
   var budgetDivStyles =
-    'margin:30px 0 0 330px; padding:0 8px 0 8px; background:rgba(253, 128, 11, 0.3); text-align:left; visibility:hidden;';
+    'margin:30px 0 0 330px; padding:0 8px 0 8px; background:rgba(253, 128, 11); text-align:left; visibility:hidden;';
   budgetDiv.setAttribute('style', budgetDivStyles);
-  budgetDiv.innerHTML = `<p style="display:inline-block; margin:4px 0 0 0;">Budget: ${budgetNote}</p>`;
+  budgetDiv.innerHTML = `
+    <p style="display:inline-block; margin:4px 0 0 0;">Users: ${userNote}</p><br/>
+    <p style="display:inline-block; margin:4px 0 0 0;">Budget: ${budgetNote}</p>
+    `;
 
   /*  Available other rendering in div:
     var sizeNote = extractNotes('cn_size')
@@ -52,9 +58,13 @@ if (window.location.href.includes(qualPage)) {
   // Add listener icon that toggles budget note visibility on click
   document.getElementById('quabity').addEventListener('click', function() {
     let budgetDiv = document.getElementById('quabity_note');
-    budgetDiv.style.visibility == 'hidden'
-      ? (budgetDiv.style.visibility = 'visible')
-      : (budgetDiv.style.visibility = 'hidden');
+    if (budgetDiv.style.visibility == 'hidden') {
+      budgetDiv.style.visibility = 'visible';
+      highlightRecs(0.3);
+    } else {
+      budgetDiv.style.visibility = 'hidden';
+      highlightRecs(0);
+    }
   });
 
   // Select Recommended Products
@@ -76,6 +86,21 @@ if (window.location.href.includes(qualPage)) {
   // Solution: use SetTimeout to delay the function.
   // Problem: Chrome API does not allow for SetTimeout use in non-persistent scripts
   // Solution: make the script persistent ;)
+
+  // Highlights recs in available list
+  function highlightRecs(num) {
+    let availRecs = document.querySelectorAll('#recommended_products .product_name > a');
+    // Loop over Available Products to pull Product Name strings
+    console.log('Recommendations within highlightRecs()');
+    console.log(Recommendations);
+    let availRecsArray = [...availRecs].map(e => {
+      Recommendations.includes(e.innerText)
+        ? e.parentNode.parentNode.setAttribute('style', `background:rgba(253, 128, 11, ${num})`)
+        : null;
+    });
+    console.log('highlightRecs');
+    return availRecsArray;
+  }
 
   // Select Available Products
   function selectAvailableProducts() {
@@ -139,10 +164,23 @@ if (window.location.href.includes(queuePage)) {
   // Create containers
   var matches = []; // will hold regex matches
   var nonMatches = []; // will hold non-matches
+  var deletedReviewed = [];
 
   // Query DOM for all table rows
   var queueData = document.querySelectorAll('#conversions .conv_name');
   var deletedQueueData = document.querySelectorAll('#conversions .conv_name_grey');
+
+  // Function to render container counts on page
+  function renderCount(arr, str, style) {
+    let wrapper = document.getElementById('conversion_filters');
+    let firstChild = wrapper.firstChild;
+    arr.length ? null : (arr = []);
+    let injHTML = `${str}: ${arr.length}`;
+    let inj = document.createElement('span');
+    inj.innerHTML = injHTML;
+    inj.setAttribute('style', `float:left; ${style}`);
+    return wrapper.insertBefore(inj, firstChild);
+  }
 
   // Loop over table rows after 2 seconds
   setTimeout(function() {
@@ -157,17 +195,30 @@ if (window.location.href.includes(queuePage)) {
 
     // Loop over 'Deleted Queue'
     [...deletedQueueData].map(e => {
-      e.innerText.search(regex) !== -1 && e.innerText.search(deletedDead) == -1 ? matches.push(e) : nonMatches.push(e);
+      if (e.innerText.search(regex) !== -1) {
+        e.innerText.search(deletedDead) !== -1 ? deletedReviewed.push(e) : matches.push(e);
+      } else {
+        nonMatches.push(e);
+      }
     });
-    // ** Add RUN or BREAK conditionality here **
 
     // Add styles to highlight each match
     matches.map(e => {
-      e.setAttribute('style', 'font-weight: bold; background: rgba(253, 128, 11, 0.3)');
+      e.setAttribute('style', 'font-weight:bold; background:rgba(253, 128, 11, 0.3)');
     });
     // Add styles to lowlight each non-match
     nonMatches.map(e => {
       e.setAttribute('style', 'opacity: .3');
     });
+    // Add styles to lowlight each deleted completed
+    deletedReviewed.map(e => {
+      e.setAttribute('style', 'opacity: .5; color:rgba(253, 128, 11) !important');
+    });
+
+    // Add <span> Totals </span> to top of deleted queue
+    if (deletedQueueData.length > 0) {
+      renderCount(deletedReviewed, 'Reviewed', 'margin-left:5%');
+      renderCount(matches, 'To Review');
+    }
   }, 1000);
 }
