a2ded1c7cd795069a42e0244ac1e74e05e546bcc
davidtraverso <davidtraverso88@gmail.com>
2020-05-25 18:46:45 -0500

    Fix rec highlight bug by trimming whitespace

--- content.js
+++ content.js
@@ -8,12 +8,12 @@ var qualPage = '/admin/qualification.php';
 if (window.location.href.includes(qualPage)) {
   /* Add observer that makes prices bold when scrolled down to recs */
   var recsDiv = document.getElementById('recommended_products');
-  
+
   window.addEventListener('load', (e) => {
     createObserver();
-    listenForNoteHover()
+    listenForNoteHover();
   });
-  
+
   function createObserver() {
     let observer;
     let options = {
@@ -98,58 +98,58 @@ if (window.location.href.includes(qualPage)) {
     }
   });
 
-
   /* Notes Scoring Feature */
   function listenForNoteHover() {
     // Remove ID from Reports tab to kill Admin's jQuery listener that interferes with the mouseenter event below
-    let reportsTab = document.getElementById('menu_reports')
+    let reportsTab = document.getElementById('menu_reports');
     reportsTab.id = '';
-    
+
     let notesIcon = document.getElementsByClassName('contains-lead-score-form');
     [...notesIcon][0].addEventListener('mouseenter', (e) => {
-      toggleNotesBox()
+      toggleNotesBox();
     });
-  };
+  }
 
   function toggleNotesBox() {
     let notesBox = document.getElementById('lead_score_form');
-    notesBox.style.display === 'block' ? notesBox.style.display = 'none' : notesBox.style.display = 'block';
+    notesBox.style.display === 'block' ? (notesBox.style.display = 'none') : (notesBox.style.display = 'block');
     scoreNotes();
   }
 
   function scoreNotes() {
     let list = document.getElementById('lead_score_form').firstElementChild;
-    [...list.children].map(listItem => {
+    [...list.children].map((listItem) => {
       listItem.addEventListener('click', (e) => {
-        e.target.dataset.score ? addScoreText(e.target.dataset.score) : addScoreText(e.target.firstElementChild.dataset.score);
+        e.target.dataset.score
+          ? addScoreText(e.target.dataset.score)
+          : addScoreText(e.target.firstElementChild.dataset.score);
         // document.getElementById('save_score').click(); <-- BUG: Excessive window.alert()s with this added
-      })
+      });
       // listItem.addEventListener('dblclick', (e) => {
       //   document.getElementById('save_score').click();
       //   e.stopPropagation();
       // })
-    })
+    });
   }
 
   function addScoreText(rating) {
     let ratingTextOptions = {
       1: `Multiple mistakes to be corrected or edits to be made by QA \nQA or advisor-required changes that affect recommendations or revenue`,
       2: `Multiple mistakes to be corrected or edits to be made by QA \nQA or advisor-required changes that do not affect recommendations or revenue \nCanned, redundant, or surface-level notes \nChats back to the advisor to clarify unclear notes or to “beef up” extremely thin notes`,
-      3:  `Minimal mistakes to be corrected or edits to be made by QA \nFree of issues that would require a chat back to the advisor \nAppropriate filters and recommendations selected \nAdequate information for recommended vendor(s) to begin the sales process`, 
+      3: `Minimal mistakes to be corrected or edits to be made by QA \nFree of issues that would require a chat back to the advisor \nAppropriate filters and recommendations selected \nAdequate information for recommended vendor(s) to begin the sales process`,
       4: `Few to no mistakes to be corrected or edits to be made by QA \n1-2 areas with unique, insightful, and thorough notes that will assist vendors in the sales process`,
-      5: `Few to no mistakes to be corrected or edits to be made by QA including but not limited to grammar, spelling (especially of product or vendor names), and contact information (including website where applicable) \nAn expanded company description including what the business does, the makeup of the company, and any other facts you have surfaced on the call. \nSize/user/employee classification with any details surfaced, no blank fields \nTimeline details \nExpanded, unique, insightful notes from second or third-level questions in at least the Shopping Around, Key Features, and Contact/Authority or Follow-up fields that will assist vendors in the sales process. Includes notes that speak to the specific impact on the business or processes and any unique tips or techniques you used successfully on the call. \nAll notes are substantive and purposeful with no filler.`
-    }
-    let textField = document.getElementsByName('score_comment')
-    textField[0].value = ratingTextOptions[rating]
+      5: `Few to no mistakes to be corrected or edits to be made by QA including but not limited to grammar, spelling (especially of product or vendor names), and contact information (including website where applicable) \nAn expanded company description including what the business does, the makeup of the company, and any other facts you have surfaced on the call. \nSize/user/employee classification with any details surfaced, no blank fields \nTimeline details \nExpanded, unique, insightful notes from second or third-level questions in at least the Shopping Around, Key Features, and Contact/Authority or Follow-up fields that will assist vendors in the sales process. Includes notes that speak to the specific impact on the business or processes and any unique tips or techniques you used successfully on the call. \nAll notes are substantive and purposeful with no filler.`,
+    };
+    let textField = document.getElementsByName('score_comment');
+    textField[0].value = ratingTextOptions[rating];
   }
-
 
   // Select Recommended Products
   function selectRecs() {
     let testRecs = document.querySelectorAll('#leads .lead_name');
     // Loop over Recommended Products to pull Product Name strings
     let recsArray = [...testRecs].map((e) => {
-      return e.innerText;
+      return e.innerText.trim();
     });
     return recsArray;
   }
@@ -167,7 +167,7 @@ if (window.location.href.includes(qualPage)) {
     let availRecs = document.querySelectorAll('#recommended_products .product_name > a');
     // Loop over Available Products to pull Product Name strings
     let availRecsArray = [...availRecs].map((e) => {
-      Recommendations.includes(e.innerText)
+      Recommendations.includes(e.innerText.trim())
         ? e.parentNode.parentNode.setAttribute('style', `background:rgba(253, 128, 11, ${num})`)
         : null;
     });
@@ -185,7 +185,6 @@ if (window.location.href.includes(qualPage)) {
   }
   var AvailableProducts;
   // Add delay to account for PHP delivery of available recs to DOM
-  
 
   // Tech Debt: Send message to background via port
   port.postMessage({
